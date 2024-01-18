1d0c1ed0759ad8349848f9d54bca8db92fc7569c
Maxim Chernyshov <dsgnrvd@gmail.com>
2024-01-18 19:30:15 +0200

    single endpoints

--- Program.cs
+++ Program.cs
@@ -46,6 +46,33 @@ app.MapGet("/get-food-items", async () => {
     return Results.Ok(foodItems);
 });
 
+app.MapGet("/get-food-items/{id}", async (int id) => {
+    await using (var cmd = dataSource.CreateCommand("SELECT * FROM FoodItems WHERE Id = @Id"))
+    {
+        cmd.Parameters.AddWithValue("@Id", id);
+
+        await using (var reader = await cmd.ExecuteReaderAsync())
+        {
+            if (await reader.ReadAsync())
+            {
+                var foodItem = new FoodItem
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.IsDBNull(1) ? null : reader.GetString(1),
+                    ImageUrl = reader.IsDBNull(2) ? null : reader.GetString(2),
+                    FoodType = reader.GetInt32(3),
+                    Fat = reader.GetDouble(4),
+                    Carbohydrates = reader.GetDouble(5),
+                    Sugar = reader.GetDouble(6),
+                    Cholesterol = reader.GetDouble(7)
+                };
+                return Results.Ok(foodItem);
+            }
+        }
+    }
+    return Results.NotFound($"Food item with id {id} not found");
+});
+
 app.MapGet("/get-food-types", async () => {
     List<FoodTypes> foodTypes = [];
     await using (var cmd = dataSource.CreateCommand("SELECT * FROM FoodTypes"))
@@ -64,6 +91,27 @@ app.MapGet("/get-food-types", async () => {
     return Results.Ok(foodTypes);
 });
 
+app.MapGet("/get-food-types/{id}", async (int id) => {
+    await using (var cmd = dataSource.CreateCommand("SELECT * FROM FoodTypes WHERE Id = @Id"))
+    {
+        cmd.Parameters.AddWithValue("@Id", id);
+
+        await using (var reader = await cmd.ExecuteReaderAsync())
+        {
+            if (await reader.ReadAsync())
+            {
+                var foodItem = new FoodTypes
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Description = reader.IsDBNull(2) ? null : reader.GetString(2)
+                };
+                return Results.Ok(foodItem);
+            }
+        }
+    }
+    return Results.NotFound($"Food item with id {id} not found");
+});
 
 app.MapPut("/update-food/{id}", async (int id, FoodItem updatedFoodItem) => {
     var cmd = dataSource.CreateCommand($"SELECT COUNT(*) FROM FoodItems WHERE Id = {id}");
